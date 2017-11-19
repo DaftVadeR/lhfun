2766d02890ba4133b644379d20e80205c37d37f5
Ross <tsuyoizuse@gmail.com>
2017-11-19 20:25:57 +0200

    Footer and header done. Moving on to content

--- index.php
+++ index.php
@@ -6,53 +6,56 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
+    <link rel="stylesheet" href="assets/css/public/app.css">
 
-    <base href="http://localhost/leadhome" />
+    <base href="http://localhost/leadhome/" />
 </head>
 <body>
-<div class="container-fluid">
-    <header id="main-header">
-        <nav class="navbar navbar-inverse navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">
-                        <div class="logo">
-                            <img src="/assets/images/logo.89cf16dfc4c9a7aaa2e15c574b9458f8.svg" class="s17v8zd0-1 kXuKHa s1lczsw5-0 bCKOwV" width="35" height="29">
-                            <span class="logo-text">Leadhome</span>
-                        </div>
-                    </a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="#">Separated link</a></li>
-                                <li><a href="#">One more separated link</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+<header id="main-header">
+    <div class="container-fluid">
+        <nav>
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="logo" href=".">
+                    <img src="assets/img/logo.svg" class="logo-img" width="35" height="29" alt="LeadHome">
+                    <span class="logo-text">Leadhome</span>
+                </a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Search properties <i class="fa fa-search col-primary" aria-hidden="true"></i></a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            More <i class="fa fa-caret-down col-primary" aria-hidden="true"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="mgn-right-sm"><a href="#">Contact Us</a></li>
+                    <li class="mgn-right-sm">
+                        <a href="#" class="btn btn-primary">Book free evaluation</a>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-border">Hi Patrick!</a>
+                    </li>
+                </ul>
             </div>
         </nav>
-    </header>
-    <section id="main-content">
+    </div>
+</header>
+<section id="main-content" class="section-pad">
+    <div class="container-fluid">
         <h1>Search Properties</h1>
 
         <div class="row">
@@ -73,13 +76,13 @@
             </div>
             <div class="col-sm-4">
                 <div class="map-search-btn input-field ">
-                    <span class="btn-left text-white">
-                        <i class="fa fa-map-marker" aria-hidden="true"></i>
-                        Map Search
-                    </span>
-                    <span class="btn-right text-dark">
-                        Discover new areas
-                    </span>
+                <span class="btn-left text-white">
+                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    Map Search
+                </span>
+                <span class="btn-right text-dark">
+                    Discover new areas
+                </span>
                 </div>
             </div>
         </div>
@@ -88,13 +91,13 @@
         </div>
 
         <div id="search-results" class="item-grid">
-            
+
         </div>
-    </section>
-</div>
+    </div>
+</section>
 <footer id="main-footer">
-    <div class="container">
-        <nav id="footer-nav">
+    <nav id="footer-nav" class="section-pad">
+        <div class="container">
             <div class="row">
                 <div class="col-sm-6 col-lg-3">
                     <h5>Leadhome</h5>
@@ -133,36 +136,30 @@
                     </ul>
                 </div>
             </div>
-        </nav>
-        <div id="footer-bot">
+        </div>
+    </nav>
+    <div id="footer-bot" class="section-pad">
+        <div class="container">
             <div class="row">
                 <div class="col-sm-7">
-                    <a class="navbar-brand" href="#">
-                        <div class="logo">
-                            <img src="/assets/images/logo.89cf16dfc4c9a7aaa2e15c574b9458f8.svg" class="s17v8zd0-1 kXuKHa s1lczsw5-0 bCKOwV" width="35" height="29">
-                            <span class="logo-text">Leadhome</span>
-                        </div>
+                    <a class="logo logo-primary pull-left mgn-right-lg" href=".">
+                        <img src="assets/img/logo.svg" class="logo-img" width="35" height="29" alt="LeadHome">
+                        <span class="logo-text">Leadhome</span>
                     </a>
                     <p>&copy; 2017 Leadhome - All rights reserved - <a href="#" class="plain-text-link">Terms of use</a></p>
                 </div>
                 <div class="col-sm-5">
                     <ul class="inline-btn-list">
-                        <li><i class="fa fa-twitter" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-facebook" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-instagram" aria-hidden="true"></i></li>
-                        <li><i class="fa fa-linkedin" aria-hidden="true"></i></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                     </ul>
                 </div>
             </div>
-            <img src="assets/img/logo.png" width="128" height="28" />
         </div>
     </div>
 </footer>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 </html>
 
